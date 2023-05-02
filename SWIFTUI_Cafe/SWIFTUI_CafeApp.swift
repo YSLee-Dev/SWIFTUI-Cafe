6207ea2fbb41c29b40f042ed9fa571f7c5579692
YoonSuLee <ysleedev01@gmail.com>
2023-05-02 18:18:28 +0900

    Initial Commit

---
+++ SWIFTUI_Cafe/SWIFTUI_CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_CafeApp.swift
+//  SWIFTUI_Cafe
+//
+//  Created by 이윤수 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
